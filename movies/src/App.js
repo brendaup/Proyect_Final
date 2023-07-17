c2b4d8e1a6a06fd6db46fe650ec174a34857dba4
Raquel_Leha <rlopezn833@gmail.com>
2023-07-17 18:20:52 +0200

    loginRegister

--- movies/src/App.js
+++ movies/src/App.js
@@ -3,13 +3,17 @@ import "./App.css";
 import { MovieContext } from "./context/MoviesContext/MoviesContext";
 import { useState } from "react";
 import Movies from "./api/movies";
-import Filter from "./components/Filter/Filter"
+import Filter from "./components/Filter/Filter";
 import DataMovies from "./components/MovieCard/DataMovies";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./Pages/Login/Login";
+import Register from "./Pages/Register/Register";
 
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
-const [dataMovies, setDataMovies] = useState([]);
-const [originalMovies, setOriginalMovies] = useState([]);
+  const [dataMovies, setDataMovies] = useState([]);
+  const [originalMovies, setOriginalMovies] = useState([]);
 
   //Follow module
 
@@ -17,19 +21,32 @@ const [originalMovies, setOriginalMovies] = useState([]);
 
   // PRUEBA
 
-
-
   return (
     <div className="App">
-      <MovieContext.Provider value={{ dataMovies, setDataMovies, followMovies, setFollowMovies, originalMovies }}>
-
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/register" element={<Register></Register>} />
+            <Route path="/login" element={<Login></Login>} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+      <MovieContext.Provider
+        value={{
+          dataMovies,
+          setDataMovies,
+          followMovies,
+          setFollowMovies,
+          originalMovies,
+        }}
+      >
         <Movies />
 
-        <Filter  />
+        <Filter />
         <DataMovies />
       </MovieContext.Provider>
     </div>
   );
 }
 
-export default App; 
+export default App;
---
+++ movies/src/api/auth.js
@@ -0,0 +1,8 @@
+import axios from './axios';
+
+
+
+export const registerRequest = (user) => axios.post("/", user);
+
+
+export const loginRequest = () => axios.get();
