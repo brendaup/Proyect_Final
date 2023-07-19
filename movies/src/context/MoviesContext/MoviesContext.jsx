eab0649391fe7bd442a4db7c9471f92216cf454c
Max <sendmailtomaxx@gmail.com>
2023-07-19 08:40:07 +0200

    fromat code

--- movies/src/context/MoviesContext/MoviesContext.jsx
+++ movies/src/context/MoviesContext/MoviesContext.jsx
@@ -10,77 +10,77 @@ export const MovieContext = createContext();
 //Exportamos un hook llamado useMovies que va a ser igual al uso del contexto
 
 export const useMovies = () => {
-    const context = useContext(MovieContext);
-  
-    if (!context) {
-      throw new Error("useMovies must be used within a TaskProvider");
-    }
-  
-    return context;
-  };
+  const context = useContext(MovieContext);
+
+  if (!context) {
+    throw new Error("useMovies must be used within a TaskProvider");
+  }
+
+  return context;
+};
 
 //Exportamos la funcion MoviesProvider que será el contenedor para todos los
 //componentes que pueden acceder
 
 export function MoviesProvider({ children }) {
-    const [movies, setMovies] = useState([]);
-    const [dataMovies, setDataMovies] = useState([]);
-    const [originalMovies, setOriginalMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const [dataMovies, setDataMovies] = useState([]);
+  const [originalMovies, setOriginalMovies] = useState([]);
   const [followMovies, setFollowMovies] = useState([]);
-  
-    const getMoviesR = async () => {
-      try {
-        const res = await getMovies();
-        setMovies(res.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-  
-    const createMovieR = async (movie) => {
-      const res = await createMovie(movie);
-      console.log("res", res);
-      
-    };
-  
-    const deleteMovieR = async (id) => {
-      const res = await deleteMovie(id);
-      console.log(res);
-    };
-  
-    const getMovieR = async (id) => {
-      const res = await getMovie(id);
-      return res.data;
-    }
-  
-    const updateMovieR = async (id, task) => {
-      const res = await updateMovie(id, task);
-      console.log(res);
-  
-    }
-  
-    return (
-      <MovieContext.Provider
-        value={{
-          movies,
-          createMovieR,
-          getMoviesR,
-          deleteMovieR,
-          getMovieR,
-          updateMovieR,
-          dataMovies,
-          setDataMovies,
-          originalMovies,
-          setOriginalMovies,
-          followMovies,
-          setFollowMovies
 
-        }}
-      >
-        {children}
-      </MovieContext.Provider>
-    );
+  const getMoviesR = async () => {
+    try {
+      const res = await getMovies();
+      setMovies(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const createMovieR = async (movie) => {
+    const res = await createMovie(movie);
+    console.log("res", res);
+
+  };
+
+  const deleteMovieR = async (id) => {
+    const res = await deleteMovie(id);
+    console.log(res);
+  };
+
+  const getMovieR = async (id) => {
+    const res = await getMovie(id);
+    return res.data;
   }
+
+  const updateMovieR = async (id, task) => {
+    const res = await updateMovie(id, task);
+    console.log(res);
+
+  }
+
+  return (
+    <MovieContext.Provider
+      value={{
+        movies,
+        createMovieR,
+        getMoviesR,
+        deleteMovieR,
+        getMovieR,
+        updateMovieR,
+        dataMovies,
+        setDataMovies,
+        originalMovies,
+        setOriginalMovies,
+        followMovies,
+        setFollowMovies
+
+      }}
+    >
+      {children}
+    </MovieContext.Provider>
+  );
+}
 
 
 
