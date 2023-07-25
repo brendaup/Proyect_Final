264427bb60635e7afd7e40742f056b9e7efe321a
brenda-armero <brendaarmeromartinez@gmail.com>
2023-07-25 12:14:47 +0200

    footer-style

--- movies/src/components/Footer/Footer.jsx
+++ movies/src/components/Footer/Footer.jsx
@@ -1,11 +1,25 @@
-
+import "./Footer.css"
 
 const Footer = () => {
   return (
     <div>
-      
+      <div className="social-container">
+        <a href="https://www.instagram.com/">
+          <i class="fa-brands fa-instagram"></i>
+        </a>
+        <a href="https://www.youtube.com/">
+          <i class="fa-brands fa-youtube"></i>
+        </a>
+        <a href="https://twitter.com/i/flow/login?redirect_after_login=%2F">
+          <i class="fa-brands fa-twitter"></i>
+        </a>
+        <a href="https://es-es.facebook.com/">
+          <i class="fa-brands fa-facebook"></i>
+        </a>
+      </div>
+      <p>© 2023 UpgradeFlix - Todos los derechos reservados</p>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
